f34e4229b8f3f4b7c022e2855901ce0f16a13f62
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-08 18:53:22 -0300

    adding favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
