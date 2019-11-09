0242f80f97137db651dccde71afcbec7c0c3546a
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-09 13:32:52 -0300

    refactoring login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully log in."
+      redirect_to root_path
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
+  end
 end
