3b979501a04bd1fb56be930a6346534e96eb9fab
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-09 18:00:30 -0300

    adding messages from UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+ // This file is automatically compiled by Webpack, along with any other files
+ // present in this directory. You're encouraged to place your actual application logic in
+ // a relevant structure within app/javascript and only use these pack files to reference
+ // that code so it'll be compiled.
 
 //= require semantic-ui
 //= require semantic-ui/dropdown
@@ -12,7 +12,8 @@
 //= require popper
 //= require rails-ujs
 
-
+require("jquery_ujs").start()
+require("semantic-ui").start()
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -27,9 +28,9 @@ $(document).on('turbolinks:load', function() {
 })
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+ // Uncomment to copy all static images under ../images to the output folder and reference
+ // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+ // or the `imagePath` JavaScript helper below.
+ //
+ // const images = require.context('../images', true)
+ // const imagePath = (name) => images(name, true)
