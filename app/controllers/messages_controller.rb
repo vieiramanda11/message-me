57d7f3c93b7fbd641481f900a40584c038e28183
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-09 20:45:22 -0300

    modifying controllers to broadcast messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,21 @@
 class MessagesController < ApplicationController
   before_action :require_user
+  skip_before_action :verify_authenticity_token
 
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel",
+                                    mod_message: message_render(message)
     end
   end
 
   private
     def message_params
       params.require(:message).permit(:body)
+    end
+
+    def message_render(message)
+      render(partial: 'message', locals: {message: message})
     end
 end
